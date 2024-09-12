b74553d97f9277315b142e8ab32532dc5cc1f6a8
nasiruddin6 <mdnasirislam208@gmail.com>
2024-09-13 01:48:26 +0600

    Problem 05:  Predict Avarage Waiting Tim

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-// Problem 04 : Calculate Admission Final Score// 
+// Problem 05:  Predict Avarage Waiting Time//
 
-function calculateFinalScore(obj) {
-  if (typeof obj !== 'object') {
-    return 'Invalid Input';
+function waitingTime(waitingTimes, serialNumber) {
+
+  if (
+    Array.isArray(waitingTimes) === false || typeof serialNumber !== 'number') {
+    return "Invalid Input";
+  }
+  let sum = 0;
+  for (let waiting of waitingTimes) {
+    sum += waiting;
   }
 
-  const testScore = obj.testScore;
-  const schoolGrade = obj.schoolGrade;
-  if (obj.isFFamily === true) {
-    isFFamily = 20;
-  } else {
-    isFFamily = 0;
-  }
-  const totalMarks = testScore + schoolGrade + isFFamily;
-  if (totalMarks >= 80) {
-    return true;
-  } else {
-    return false;
-  }
+  const Length = waitingTimes.length;
+  const avarageTotal = sum / Length;
+  const round = Math.round(avarageTotal);
+
+  const item = serialNumber - 1;
+  const LaterItem = item - Length;
+
+  const wait_Time = round * LaterItem;
+
+  return wait_Time;
+
 }
 
-const info = { name: 'Rajib', testScore: 15, schoolGrade: 25, isFFamily: true };
-const results = calculateFinalScore(info);
-console.log(results);
+const arrayTime = [ 6 ];
+const serialNumbers =4;
 
-
-/**
- * 
- */
+const time_result = waitingTime(arrayTime, serialNumbers);
+console.log(time_result);
