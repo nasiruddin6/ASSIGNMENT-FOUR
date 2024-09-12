0ec647a1260837bac27d6637fb5689e7145f75ac
nasiruddin6 <mdnasirislam208@gmail.com>
2024-09-13 01:50:09 +0600

    all reposite file

--- index.js
+++ index.js
@@ -1,3 +1,101 @@
+// Problem-01 : Tax Calculator//
+
+
+function calculateTax(income, expense) {
+  if (income < 0 || expense < 0 || income < expense) {
+    return "Invalid Input";
+  }
+  const difference = income - expense;
+  const tax = difference * .20;
+  return tax;
+}
+const result = calculateTax(10000, 3000);
+console.log(result);
+
+
+/**
+ * 
+ */
+
+
+// Problem 02 : Notification Generator//
+
+function sendNotification(email) {
+  if (!email.includes('@')) {
+    return "Invalid Email";
+  }
+
+  const email_name = email.split("@")
+  const nickName = email_name[0]
+  const domaineName = email_name[1]
+
+  const notification = nickName + " sent you an email from " + domaineName
+
+  return notification
+
+}
+console.log(sendNotification("nurmahammadmondol@gmail.com"));
+console.log(sendNotification("mdlitonmiagmail.com"));
+
+
+/**
+ * 
+ */
+
+// Problem-03: Checking Digits Inside a Name//
+
+function checkDigitsInName(name) {
+  if (typeof name !== 'string') {
+    return "Invalid Input";
+  }
+  let digits = false;
+  for (let i = 0; i < name.length; i++) {
+    if (!isNaN(name[i]) && name[i] !== ' ') {
+      digits = true;
+      break;
+    }
+  }
+  return digits;
+}
+console.log(checkDigitsInName(898));
+
+/**
+ * 
+ */
+
+
+// Problem 04 : Calculate Admission Final Score// 
+
+function calculateFinalScore(obj) {
+  if (typeof obj !== 'object') {
+    return 'Invalid Input';
+  }
+
+  const testScore = obj.testScore;
+  const schoolGrade = obj.schoolGrade;
+  if (obj.isFFamily === true) {
+    isFFamily = 20;
+  } else {
+    isFFamily = 0;
+  }
+  const totalMarks = testScore + schoolGrade + isFFamily;
+  if (totalMarks >= 80) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const info = { name: 'Rajib', testScore: 15, schoolGrade: 25, isFFamily: true };
+const results = calculateFinalScore(info);
+console.log(results);
+
+
+/**
+ * 
+ */
+
+
 // Problem 05:  Predict Avarage Waiting Time//
 
 function waitingTime(waitingTimes, serialNumber) {
